e269d82755b9dc8a797fde62062d4a3a84a3bf7f
Gabrielle Kelsey <gabriellemkelsey@gmail.com>
2025-06-22 15:19:36 +0800

    Modify source code to accept two connections (2)

---
+++ LA4-SocketProgrammingTask2/ChatServer.java
@@ -0,0 +1,59 @@
+import java.net.*;
+import java.io.*;
+
+public class ChatServer
+{
+	public static void main(String[] args)
+	{
+		int nPort = Integer.parseInt(args[0]);
+		System.out.println("Server: Listening on port " + args[0] + "...\n");
+		ServerSocket serverSocket;
+
+		try 
+		{
+			serverSocket = new ServerSocket(nPort);
+
+            Socket clientA = serverSocket.accept();
+			System.out.println("Server: New client connected: " + clientA.getRemoteSocketAddress() + "\n");
+
+            System.out.println("Server: Listening on port " + args[0] + "...\n");
+			
+            Socket clientB = serverSocket.accept();
+			System.out.println("Server: New client connected: " + clientB.getRemoteSocketAddress() + "\n");
+			
+            /* Data Stream for Client A */
+			DataInputStream disReaderA = new DataInputStream(clientA.getInputStream());
+			DataOutputStream dosWriterA = new DataOutputStream(clientA.getOutputStream());
+			
+            /* Data Stream for Client B */
+			DataInputStream disReaderB = new DataInputStream(clientB.getInputStream());
+			DataOutputStream dosWriterB = new DataOutputStream(clientB.getOutputStream());
+
+            /* Client Names */
+            String nameClientA = disReaderA.readUTF();
+            String nameClientB = disReaderB.readUTF();
+
+            /* Client Messages */
+            String messageClientA = disReaderA.readUTF();
+            String messageClientB = disReaderB.readUTF();
+
+            /* Message Sending to Recipient */
+            String responseClientA = "Message from " + nameClientB + ": " + messageClientB + "\n";
+			dosWriterA.writeUTF(responseClientA);
+
+            String responseClientB = "Message from " + nameClientA + ": " + messageClientA + "\n";
+			dosWriterB.writeUTF(responseClientB);
+
+			clientA.close();
+			clientB.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			System.out.println("Server: Connection terminated.");
+		}
+	}
+}
