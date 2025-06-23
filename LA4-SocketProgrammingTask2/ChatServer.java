76c114ac829126559d71de8d33b6f0fc764bfc4a
haniellejermayn <chua.haniellejermayn@gmail.com>
2025-06-23 10:23:08 +0800

    fix: resolved socket not closed warning

--- LA4-SocketProgrammingTask2/ChatServer.java
+++ LA4-SocketProgrammingTask2/ChatServer.java
@@ -1,58 +1,58 @@
 import java.net.*;
 import java.io.*;
 
-public class ChatServer
-{
-	public static void main(String[] args)
-	{
+public class ChatServer {
+	public static void main(String[] args) {
 		int nPort = Integer.parseInt(args[0]);
 		System.out.println("Server: Listening on port " + args[0] + "...\n");
-		ServerSocket serverSocket;
+		ServerSocket serverSocket = null;
 
-		try 
-		{
+		try {
 			serverSocket = new ServerSocket(nPort);
 
-            Socket clientA = serverSocket.accept();
+			Socket clientA = serverSocket.accept();
 			System.out.println("Server: New client connected: " + clientA.getRemoteSocketAddress() + "\n");
 
-            System.out.println("Server: Listening on port " + args[0] + "...\n");
-			
-            Socket clientB = serverSocket.accept();
+			System.out.println("Server: Listening on port " + args[0] + "...\n");
+
+			Socket clientB = serverSocket.accept();
 			System.out.println("Server: New client connected: " + clientB.getRemoteSocketAddress() + "\n");
-			
-            /* Data Stream for Client A */
+
+			/* Data Stream for Client A */
 			DataInputStream disReaderA = new DataInputStream(clientA.getInputStream());
 			DataOutputStream dosWriterA = new DataOutputStream(clientA.getOutputStream());
-			
-            /* Data Stream for Client B */
+
+			/* Data Stream for Client B */
 			DataInputStream disReaderB = new DataInputStream(clientB.getInputStream());
 			DataOutputStream dosWriterB = new DataOutputStream(clientB.getOutputStream());
 
-            /* Client Names */
-            String nameClientA = disReaderA.readUTF();
-            String nameClientB = disReaderB.readUTF();
+			/* Client Names */
+			String nameClientA = disReaderA.readUTF();
+			String nameClientB = disReaderB.readUTF();
 
-            /* Client Messages */
-            String messageClientA = disReaderA.readUTF();
-            String messageClientB = disReaderB.readUTF();
+			/* Client Messages */
+			String messageClientA = disReaderA.readUTF();
+			String messageClientB = disReaderB.readUTF();
 
-            /* Message Sending to Recipient */
-            String responseClientA = "Message from " + nameClientB + ": " + messageClientB + "\n";
+			/* Message Sending to Recipient */
+			String responseClientA = "Message from " + nameClientB + ": " + messageClientB + "\n";
 			dosWriterA.writeUTF(responseClientA);
 
-            String responseClientB = "Message from " + nameClientA + ": " + messageClientA + "\n";
+			String responseClientB = "Message from " + nameClientA + ": " + messageClientA + "\n";
 			dosWriterB.writeUTF(responseClientB);
 
 			clientA.close();
 			clientB.close();
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally
-		{
+		} finally {
+			if (serverSocket != null && !serverSocket.isClosed()) {
+				try {
+					serverSocket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 			System.out.println("Server: Connection terminated.");
 		}
 	}
