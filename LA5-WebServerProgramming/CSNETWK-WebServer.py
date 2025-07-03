6697662c2f02c146944fa814ca8d459cbfcfc1d5
Kelsey <gabriellemkelsey@gmail.com>
2025-07-03 17:49:35 +0800

    Filled in missing code

--- LA5-WebServerProgramming/CSNETWK-WebServer.py
+++ LA5-WebServerProgramming/CSNETWK-WebServer.py
@@ -1,25 +1,26 @@
-#import socket module
+# import socket module
 from socket import *
 import sys # In order to terminate the program
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
-#Prepare a sever socket
-#Fill in start
-#Fill in end
+# Prepare a sever socket
+serverSocket.bind(('', 6969))  # Fill
+serverSocket.listen(1)          # Fill
 
 
 while True:
     #Establish the connection
     print('CSNETWK Web Server is ready to serve...')
-    connectionSocket, addr = #Fill in start     #Fill in end
+    connectionSocket, addr = serverSocket.accept()      # Fill
     try:
-        message = #Fill in start #Fill in end
+        message = connectionSocket.recv(1024).decode()  # Fill
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start #Fill in end
+        outputdata = f.read()   # Fill
+        
         #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())   # Fill
+        
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
@@ -29,12 +30,11 @@ while True:
     
     except IOError:
         #Send response message for file not found
-        #Fill in start
-        #Fill in end
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())    # Fill
+        connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>".encode()) # Fill
        
         #Close client socket
-        #Fill in start
-        
-        #Fill in end
+        connectionSocket.close() # Fill
+
 serverSocket.close()
-sys.exit()  #Terminate the program after sending the corresponding data
+sys.exit()  # Terminate the program after sending the corresponding data
